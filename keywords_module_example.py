db6a368bf4b73e2a7615c2ef2a06786affabf4a9
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2024-02-28 09:50:20 +0000

    Add keyword and operator module programs

---
+++ keywords_module_example.py
@@ -0,0 +1,8 @@
+import keyword
+
+print("'def' is a keyword: ", keyword.iskeyword('def'))
+print("'mef' is a keyword: ", keyword.iskeyword('mef'))
+
+print("Keywords list: ", keyword.kwlist)
+
+print("Soft keywords list: ", keyword.softkwlist)
---
+++ operator_module_example.py
@@ -0,0 +1,12 @@
+import operator
+
+print("operator.add(2, 3): ", operator.add(2, 3))
+print("operator.sub(2, 3): ", operator.sub(2, 3))
+print("operator.mul(2, 3): ", operator.mul(2, 3))
+print("operator.truediv(2, 3): ", operator.truediv(2, 3))
+print("operator.floordiv(2, 3): ", operator.floordiv(2, 3))
+print("operator.lt(2, 3): ", operator.lt(2, 3))
+print("operator.gt(2, 3): ", operator.gt(2, 3))
+print("operator.le(2, 3): ", operator.le(2, 3))
+print("operator.ge(2, 3): ", operator.ge(2, 3))
+print("operator.pow(2, 3): ", operator.pow(2, 3))
