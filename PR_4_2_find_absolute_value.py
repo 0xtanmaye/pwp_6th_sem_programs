cbd2a8083b056e2b4b4a1efd50f17e9e1876cd1b
0xtanmaye <106451224+0xtanmaye@users.noreply.github.com>
2024-03-25 18:47:58 +0530

    Update PR_4_2_find_absolute_value.py with indentation fix

--- PR_4_2_find_absolute_value.py
+++ PR_4_2_find_absolute_value.py
@@ -2,5 +2,5 @@ num = float(input("Enter a number to find it's absolute value: "))
 if (num < 0):
 	abs_num = num * -1
 else:
-    abs_num = num
+	abs_num = num
 print(f"Absolute value of {num} = {abs_num}")
